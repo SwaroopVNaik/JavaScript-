bef493ddc5ac91174bc6823746560928409d1c6c
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-02 21:09:32 +0530

    DataTypes

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treats js code as newer version
+
+// alert ( 3 + 3 ) // we are using node.js, not browser
+
+// code readablititly should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Pimitive Data types :>
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbols => unique
+
+// Object
+
+console.log(typeof "swaroop");
+console.log(typeof null); // null is a object
+console.log(typeof undefined); // undefined is undefined 
