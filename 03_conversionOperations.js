d950ab336e647bc6be07c2f96ac83c8c90a9ad3d
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-03 19:06:24 +0530

    converstion_of_dataTypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,51 @@
+let score = "33"
+let Anonymous_Value = "33abc"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+let Anonymous_ValueInNumber = Number(Anonymous_Value)
+//console.log(typeof Anonymous_ValueInNumber);
+
+let testing_1 = undefined
+
+//console.log(testing_1);
+
+//console.log(typeof testing_1);
+
+let testing_2 = true
+
+//console.log(testing_2);
+
+//console.log(typeof testing_2);
+
+let converstion_testing_1 = Number(testing_1)
+
+//console.log(converstion_testing_1)
+
+//console.log(typeof converstion_testing_1)
+
+
+// when we convert "33" to Number => it easily converts to 33
+// similalry "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false 
+// "" => false ; "swaroop"  => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+ 
