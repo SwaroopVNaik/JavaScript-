869de41020504f1353da0be9adb43a879a27d450
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-03 20:02:49 +0530

    Convertion_Operations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -36,7 +36,7 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false 
 // "" => false ; "swaroop"  => true
@@ -45,7 +45,98 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
- 
+// ***************************** operations ************************************
+
+// A string is a sequence of characters (letters, numbers, symbols, or spaces) enclosed in quotation marks.
+
+// An array is a collection of elements stored in contiguous memory locations and accessed using an index.
+
+let Value = 3
+let Neg_Value = -Value
+
+// console.log(Neg_Value);
+
+// console.log(2 + 2);
+// console.log(2 - 3);
+// console.log(2 * 3);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Hello"
+let str2 = " Swaroop"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// case 1 =>
+// console.log("1" + 2);
+
+// case 2 =>
+// console.log(1 + "2");
+
+// case 3 =>
+// console.log("1" + "2");
+
+// case 4 =>
+// console.log("1" + 2 + 2);
+
+// case 5 =>
+// console.log(1 + 2 + "2");
+
+// case 6 =>
+// console.log(+true);
+
+// // case 7 
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter);
+
+
+// don't follow the above conversion syntax methods , this is just for showing how messy code will confuse us, so always write a good readable code
+
+// Prefix and Postfix Example in One Program
+
+let a = 5;
+let b = 5;
+
+// Postfix increment
+// First uses the value, Then increases it
+
+
+let resultPostfix = a++;
+console.log("Postfix:");
+console.log("Value of resultPostfix:", resultPostfix); // 5
+console.log("Value of a after increment:", a); // 6
+
+console.log("-------------------");
+
+// Prefix increment
+// First increases the value Then uses it
+
+
+let resultPrefix = ++b;
+console.log("Prefix:");
+console.log("Value of resultPrefix:", resultPrefix); // 6
+console.log("Value of b after increment:", b); // 6
+
+
+
+
+
+
+
+
+
+
