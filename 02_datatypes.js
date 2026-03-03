e6250e1fe8de8d40fdf1448aa0338eaa3410473f
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-03 11:30:38 +0530

    datatypes

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -2,11 +2,11 @@
 
 // alert ( 3 + 3 ) // we are using node.js, not browser
 
-// code readablititly should be high
+// code readablititly should be higher
 
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
 let state;
 
 // Pimitive Data types :>
@@ -18,8 +18,13 @@ let state;
 // undefined => 
 // symbols => unique
 
-// Object
+// Non - primitive Data Types
+// Object ---> js array ---> js object
+
+//typeof -> to check the the datatype ! 
 
 console.log(typeof "swaroop");
 console.log(typeof null); // null is a object
 console.log(typeof undefined); // undefined is undefined 
+
+console.log(process.version);
