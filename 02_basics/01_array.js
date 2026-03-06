b1234595459bce9d4018cd15a3c28d0037cdea2f
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-06 22:40:27 +0530

    JS

---
+++ 02_basics/01_array.js
@@ -0,0 +1,7 @@
+// Array
+// An array is a linear data structure that stores multiple elements of the same data type in contiguous memory locations, and each element can be accessed using its index.
+// Array is JavaScript is re - sizeable.
+
+
+const myArray = [0, 1, 2, 3, 4, 5]
+
