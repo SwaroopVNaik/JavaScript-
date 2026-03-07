5dd9692fee72e67f7cb309c7970cb095c043100f
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-07 22:15:28 +0530

    JS

---
+++ 02_basics/03_Objects.js
@@ -0,0 +1,8 @@
+/* 
+In JavaScript, an object is a data structure used to store collections of related data and functions. The data is stored as key–value pairs, where:
+
+Key (property name) → identifies the value
+
+Value → can be any data type (string, number, array, function, another object, etc.) 
+*/
+
