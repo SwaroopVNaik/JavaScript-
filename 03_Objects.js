d8a5f7629c709f79146905a5f7e74b23473bace3
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-07 22:16:02 +0530

    Delete

--- 03_Objects.js
+++
@@ -1,8 +0,0 @@
-/* 
-In JavaScript, an object is a data structure used to store collections of related data and functions. The data is stored as key–value pairs, where:
-
-Key (property name) → identifies the value
-
-Value → can be any data type (string, number, array, function, another object, etc.) 
-*/
-
