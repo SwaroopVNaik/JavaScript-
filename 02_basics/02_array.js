92f9e786a0b195dbb9b7a26c0b02506ee1202004
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-07 21:55:24 +0530

    JS

---
+++ 02_basics/02_array.js
@@ -0,0 +1,20 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// push => joins string => ["thor", "ironman", "spiderman", ["superman", "flash", "batman"]]
+
+//marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// concat => ["thor", "ironman", "spiderman", "superman", "flash", "batman"]
+
+const all_heros = marvel_heros.concat(dc_heros)
+
+console.log(all_heros);
+
+// Spread => 
+
+
+
