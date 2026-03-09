96de02f52d3adb00d45a121d2cc8b54fe62fcfd6
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-09 13:17:56 +0530

    JS

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -49,4 +49,51 @@ function loginUserMessage(username){
 // when we dont define/pass value in function we get undefined
 console.log( loginUserMessage());
 
+// ... rest operator/spread Operator
+
+function CalculateCartPrice(...num_1){
+    return num_1
+}
+
+console.log(CalculateCartPrice(200, 400, 500, 2000));
+
+// case 2
+
+function CalculateCartPriceV2(val1, val2, ...num_1){
+    return num_1
+}
+
+console.log(CalculateCartPriceV2(200, 400, 500, 2000));
+
+// Objects 
+const User = {
+    username : "swaroop",
+    price : 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+
+// case 3
+handleObject(User)
+
+//case 2
+handleObject({
+    username : "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(GetArray){
+    return GetArray[1]
+}
+
+// case 1 :
+console.log(returnSecondValue(myNewArray));
+
+// case 2 :
+console.log(returnSecondValue([200, 300, 500, 1000]));
 
