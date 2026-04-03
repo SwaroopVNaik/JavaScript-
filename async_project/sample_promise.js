9bc8fec6b60a02e6f3e60fda16ea031049d6fadf
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-03 11:55:51 +0530

    sample promise

---
+++ async_project/sample_promise.js
@@ -0,0 +1,11 @@
+let empRegistration = ()=>{
+
+    return new Promise((success,failure)=>{
+        setTimeout(()=>{},4000)
+    }
+    );
+}
+
+empRegistration()
+.then((resp)=>{})
+.catch((err)=>{})
