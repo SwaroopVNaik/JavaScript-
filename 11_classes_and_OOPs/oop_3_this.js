3fe62c3eb2c04b336f6e7bece4c1b3fada049a3b
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-08 11:35:48 +0530

    OOP_3

--- 11_classes_and_OOPs/oop_3_this.js
+++ 11_classes_and_OOPs/oop_3_this.js
@@ -2,13 +2,19 @@ class Account{
     acc_bal = 0;
 
     deposit_Amount(amount){
+        // The this keyword is a special reference used in object-oriented programming languages to represent the current object — the one whose method or constructor is being executed.
+        // To access class memebers inside class we use this keyword
+
         this.acc_bal = this.acc_bal + amount;
     }
 }
 
+// to acces methods outside class we use Objects 
+// Declaring new Objects
 let A1 = new Account();
 
 
+// Callling methods
 A1.deposit_Amount(500)
 A1.deposit_Amount(1000)
 
